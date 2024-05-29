fb0e87ed981770d7544086ae836544a4c41c65c3
codinghubindia <manjun2412@gmail.com>
2024-05-29 15:00:28 +0530

    Final Design

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -5,15 +5,17 @@ import { logo1, logo2, logo3, logo4, logo5, logo6 } from '../imex';
 import { unlock, ricon1, ricon2, ricon3, ricon4, tesla, c1, c2, c3 } from '../imex';
 
 const Body = () => {
-  useIntersectionObserver('.animated-section', { threshold: 0.1 });
+  useIntersectionObserver('.animated-section-left', { threshold: 0.1 });
+  useIntersectionObserver('.animated-section-right', { threshold: 0.1 });
+
 
   return (
     <div>
-      <div className="unlock animated-section">
-        <div className="unlockImg">
+      <div className="unlock">
+        <div className="unlockImg animated-section-left">
           <img src={unlock} alt="Unlock" />
         </div>
-        <div className="unlockContent">
+        <div className="unlockContent animated-section-right">
           <div className="unlockDescription">
             <h3>The Unseen of spending three<br />years at Pixelgrade</h3><br/>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa eum error earum nesciunt iste aperiam est animi impedit, aut deserunt quisquam quasi ipsam distinctio. Fuga sit aperiam iusto totam, possimus quam voluptas numquam beatae adipisci ab ducimus officia voluptatem repudiandae architecto animi dolorem vero! Sint maiores nulla odio commodi, recusandae vero. Natus quae fugiat itaque enim molestiae laboriosam consequuntur labore?</p>
@@ -23,12 +25,12 @@ const Body = () => {
           </div>
         </div>
       </div>
-      <div className="achievements animated-section">
-        <div className="sectionHeading">
+      <div className="achievements">
+        <div className="sectionHeading animated-section-left">
           <h3>Helping a local<br /><span>business reinvent itself</span></h3>
           <p>We reached here with Our hard work and dedicaiton</p>
         </div>
-        <div className="counts">
+        <div className="counts animated-section-right">
           <div className="row">
             <div className="r1">
               <div className="Icon">
@@ -59,11 +61,11 @@ const Body = () => {
           </div>
         </div>
       </div>
-      <div className="calender animated-section">
-        <div className="lock">
+      <div className="calender">
+        <div className="lock animated-section-left">
           <img src={unlock} alt="" />
         </div>
-        <div className="lockContent">
+        <div className="lockContent animated-section-right">
           <div className="lockDescription">
             <h2>How to design your site footer like<br />we did</h2>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum, est debitis cupiditate architecto non earum consequatur harum expedita molestias optio dolor beatae temporibus quidem. Optio natus vel iusto modi? Eius! Labore deleniti nobis sint vitae ullam, fuga eos quisquam soluta delectus rem, natus modi dolores sequi error optio! At illo iusto voluptatum amet deleniti minus impedit distinctio! Ipsa, possimus alias.</p>
@@ -73,11 +75,11 @@ const Body = () => {
           </div>
         </div>
       </div>
-      <div className="customers animated-section">
-        <div className="customerImg">
+      <div className="customers">
+        <div className="customerImg animated-section-left">
           <img src={tesla} alt="" />
         </div>
-        <div className="customerContent">
+        <div className="customerContent animated-section-right">
           <div className="customerDescription">
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae ab aut, ipsa dignissimos adipisci nulla reiciendis aliquid velit. Accusantium odit pariatur aliquid magnam itaque debitis? Ratione explicabo delectus ipsam voluptatum. Minus ullam temporibus neque qui eaque? Velit, itaque totam voluptas nobis repudiandae sit quos aliquid a, distinctio perferendis dolorum magnam ducimus cupiditate qui doloribus nesciunt eligendi neque, dolore accusamus nostrum.</p>
             <h5>Tim Smith</h5>
@@ -94,12 +96,12 @@ const Body = () => {
           </div>
         </div>
       </div>
-      <div className="communityUpdate animated-section">
-        <div className="communitybdy">
+      <div className="communityUpdate">
+        <div className="communitybdy animated-section-left">
           <h3>Caring is the new marketing</h3>
-          <p>The Nextcent blog is the best place to read about the latest membership insights, trends and more. See who's joining the community, read about how our community are increasing their membership income and lot's more.​</p>
+          <p>The Nextcent blog is the best place to read about the latest membership insights,<br/> trends and more. See who's joining the community, read about how our community<br/> are increasing their membership income and lot's more.​</p>
         </div>
-        <div className="ctyframe">
+        <div className="ctyframe animated-section-right">
           <div className="f">
             <div className="img"><img src={c1} alt="" /></div>
             <div className="contentcard">
